93d9fbf2d30be965047df1134bfe5e563b8a5384
linjiabao001 <linjiabao001@ke.com>
2021-10-01 14:43:44 +0800

    project init

---
+++ web/src/main/java/com/github/krystalics/d10/scheduler/web/WebApplication.java
@@ -0,0 +1,13 @@
+package com.github.krystalics.d10.scheduler.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebApplication.class, args);
+    }
+
+}
