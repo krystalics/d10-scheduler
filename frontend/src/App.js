9c95cf2eeffee26c4f25431eb0d9141c254e2c5b
linjiabao001 <linjiabao001@ke.com>
2021-10-08 11:44:56 +0800

    前端加入战场

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import { Button } from 'antd';
+
+const App = () => (
+    <div className="App">
+      <Button type="primary">Button</Button>
+    </div>
+);
+
+
+export default App;
